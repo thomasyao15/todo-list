cdee04af22910b182aefac114623d574a7e15180
Thomas <thomasyao15@gmail.com>
2021-08-14 23:13:54 +1000

    Added logic to core objects and added static classes for logic and display controllers

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 initPage();
 
+function generateUniqueID() {
+    return uuidv4();
+}
+
 
 class Todo {
     constructor(title) {
         this.title = title;
         this.completed = false;
+        this.ID = generateUniqueID();
     }
 
     toggleCompletion() {
@@ -16,18 +21,27 @@ class Todo {
     }
 }
 
+
 class Project {
     constructor(title) {
         this.title = title;
         this.todos = [];
+        this.ID = generateUniqueID();
     }
 
     addTodo(todo) {
         this.todos.push(todo);
     }
 
-
+    deleteTodo(todoID) {
+        this.todos.forEach((todo, index, array) => {
+            if (todo.ID == todoID) {
+                array.splice(index, 1);
+            }
+        });
+    }
 }
+
 
 class User {
     constructor() {
@@ -38,5 +52,59 @@ class User {
         this.projects.push(project);
     }
 
+    deleteTodo(projectID) {
+        this.projects.forEach((project, index, array) => {
+            if (project.ID == projectID) {
+                array.splice(index, 1);
+            }
+        });
+    }
+
     // TODO: get todos from browser saved files
+}
+
+
+class LogicController {
+    static initNewSession() {
+        // TODO: make new user, check if browser has local saves, if not:
+        // create project and todos objects to populate for demo
+        // add init page logic into here so index.js just calls this method
+        // WRITE BELOW METHODS FIRST, then call them so you dont repeat code
+    }
+
+    static handleCreateProjectClick() {
+        // TODO: entry point to creating new peojects
+        // Call this from init new session
+    }
+
+    static handleDeleteProjectClick() {
+    }
+
+    static handleChangeProjectClick() {
+    }
+
+    static handleCreateTodoClick() {
+    }
+
+    static handleDeleteTodoClick() {
+    }
+
+    static handleToggleTodoClick() {
+    }
+}
+
+
+class DisplayController {
+    static currentProjectTab = "";  // TODO: insert default project ID in here
+
+    static renderProject(projectID) {
+    }
+
+    static addTodo() {
+    }
+
+    static deleteTodo() {
+    }
+
+    // TODO: add other methods as required
 }
